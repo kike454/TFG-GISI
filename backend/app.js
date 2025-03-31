28c186afa398e854cf48374300706973ffc16276
kike454 <enriquecomu5@gmail.com>
2025-03-31 23:11:33 +0200

    CI/CD initial configuration

--- backend/app.js
+++ backend/app.js
@@ -10,8 +10,8 @@ var usersRouter = require('./routes/users');
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+//app.set('views', path.join(__dirname, 'views'));
+//app.set('view engine', 'ejs');
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -28,14 +28,16 @@ app.use(function(req, res, next) {
 });
 
 // error handler
+//comportamiento como API
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
+  
+  res.status(err.status || 500).json({
+    message: err.message || 'Internal Server Error',
+    error: err
+  });
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
 });
 
 module.exports = app;
