48de05771011f1ca5322efb48a9640f984c94e86
kike454 <enriquecomu5@gmail.com>
2025-04-26 12:06:37 +0200

    troubleshooting problems with the cloud session

--- backend/app.js
+++ backend/app.js
@@ -65,9 +65,10 @@ app.use(session({
   resave: false,
   saveUninitialized: false,
   cookie: {
-    maxAge: 24 * 60 * 60 * 1000, 
-    secure: false,
-    httpOnly: true
+    maxAge: 24 * 60 * 60 * 1000,  
+    secure: false,               
+    httpOnly: true,
+    sameSite: 'Lax'               
   }
 }));
 
